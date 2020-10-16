8e4d840426b8a7a1a7ff5978f796e4fe5f41054c
rezaahmadk <rezaahmadk@gmail.com>
2020-10-16 14:19:33 +0700

    Implement Auth Service

---
+++ auth-service/go.mod
@@ -0,0 +1,8 @@
+module github.com/rezaahmadk/dts-microservice/auth-service
+
+go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/rezaahmadk/dts-microservice v0.0.0-20201015033134-eebc142286c2
+)
--- menu-service/main.go
+++ menu-service/main.go
@@ -26,9 +26,12 @@ func main() {
 
 	router := mux.NewRouter()
 
+	authMiddleware := handler.AuthMiddleware{
+		AuthService: cfg.AuthService,
+	}
 	menuHandler := handler.Menu{Db: db}
 
-	router.Handle("/add-menu", http.HandlerFunc(menuHandler.AddMenu))
+	router.Handle("/add-menu", authMiddleware.ValidateAuth(http.HandlerFunc(menuHandler.AddMenu)))
 	router.Handle("/menu", http.HandlerFunc(menuHandler.GetAllMenu))
 
 	fmt.Printf("Server Listen On :%s", cfg.Port)
